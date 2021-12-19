936f2f38790117baeea6fd2173a9f64d1757096a
samabraha <samson.abraha@gmail.com>
2021-12-19 18:41:56 +0100

    OpenGL preliminaries

--- OpenGLCherno/test.c
+++ OpenGLCherno/test.c
@@ -22,6 +22,12 @@ int main(void)
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
 

2c3cb691942e161f2516d2fd60f8be771a3144c0
samabraha <samson.abraha@gmail.com>
2021-12-29 15:35:32 +0100

    Searching in a BST

--- commonsense_dsa/chapter_15/binary_search_tree.py
+++ commonsense_dsa/chapter_15/binary_search_tree.py
@@ -12,8 +12,17 @@ class TreeNode:
         else:
             return search(searchValue, node.rightChild)
 
-    
-
+    def insert(value, node):
+        if value < node.value:
+            if node.leftChild is None:
+                node.leftChild = TreeNode(value)
+            else:
+                insert(value, node.leftChild)
+        elif value > node.value:
+            if node.rightChild is None:
+                node.rightChild = TreeNode(value)
+            else:
+                insert(value, node.rightChild)
 
 
 node1 = TreeNode(25)

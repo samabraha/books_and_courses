671eaddcfc9e87aaafeccb39fcd5e2b30a4b7bb6
samabraha <samson.abraha@gmail.com>
2022-01-01 07:38:25 +0100

    Trie.search() implementation

--- commonsense_dsa/chapter_17/trie.py
+++ commonsense_dsa/chapter_17/trie.py
@@ -6,3 +6,14 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
     
+    def search(self, word):
+        currentNode = self.root
+
+        for char in word:
+            if currentNode.children.get(char):
+                currentNode = currentNode.children[char]
+            else:
+                return None
+        return currentNode
+    
+    

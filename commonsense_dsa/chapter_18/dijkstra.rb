aca5c585d2770db78d17033b88139dec3fea17ef
samabraha <samson.abraha@gmail.com>
2022-01-06 14:40:07 +0100

    dijkstra's algorithm

--- commonsense_dsa/chapter_18/dijkstra.rb
+++ commonsense_dsa/chapter_18/dijkstra.rb
@@ -3,35 +3,19 @@ class City
 
     def initialize(name)
         @name = name
-        @route = {}
+        @routes = {}
     end
 
-    def add_route(City, price)
-        @route[City] = price
+    def add_route(city, price)
+        @routes[city] = price
     end
 end
 
- atlanta = City.new("Atlanta")
- boston = City.new("Boston")
- chicago = City.new("Chicago")
- denver = City.new("Denver")
- el_paso = City.new("El Paso")
- 
- atlanta.add_route(boston, 100)
- atlanta.add_route(denver, 0)
- boston.add_route(chicago, 0)
- boston.add_route(denver, 0)
- chicago.add_route(el_paso, 0)
- denver.add_route(chicago, 0)
- denver.add_route(el_paso, 0)
- 
- 
- def dijkstra_shortest_path(starting_city, final_destination)
+def dijkstra_shortest_path(starting_city, final_destination)
     cheapest_prices_table = {}
     cheapest_previous_stopover_city_table = {}
 
     unvisited_cities = []
-
     visited_cities = {}
 
     cheapest_prices_table[starting_city.name] = 0
@@ -42,7 +26,63 @@ end
         visited_cities[current_city.name] = true
         unvisited_cities.delete(current_city)
 
-        
+        current_city.routes.each do |adjacent_city, price|
+            unvisited_cities << adjacent_city unless visited_cities[adjacent_city.name]
+
+            price_through_current_city = cheapest_prices_table[current_city.name] + price
+
+            if !cheapest_prices_table[adjacent_city.name] || 
+                    price_through_current_city < cheapest_prices_table[adjacent_city.name]
+
+                cheapest_prices_table[adjacent_city.name] = price_through_current_city
+                cheapest_previous_stopover_city_table[adjacent_city.name] = current_city.name
+            end
+        end
+
+        current_city = unvisited_cities.min do |city|
+            cheapest_prices_table[city.name]
+        end
     end
 
+    shortest_path = []
+
+    current_city_name = final_destination.name
+
+    while current_city_name != starting_city.name
+        shortest_path << current_city_name
+
+        current_city_name = cheapest_previous_stopover_city_table[current_city_name]
+    end
+
+    shortest_path << starting_city.name
+
+    return shortest_path.reverse
  end
+
+asmara = City.new("asmara")
+keren = City.new("keren")
+mendefera = City.new("mendefera")
+barentu = City.new("barentu")
+massawa = City.new("massawa")
+assab = City.new("assab")
+adi_keyh = City.new("adi_keyh")
+teseney = City.new("teseney")
+dekemhare = City.new("dekemhare")
+akordat = City.new("akordat")
+adikuala = City.new("adikuala")
+gindae = City.new("gindae")
+
+asmara.add_route(keren, 91)
+asmara.add_route(mendefera, 54)
+# asmara.add_route(adi_keyh, 114)
+keren.add_route(barentu, 67)
+# mendefera.add_route(adikuala, 80)
+# barentu.add_route(akordat, 60)
+# massawa.add_route(asmara, 115)
+# assab.add_route(massawa, 800)
+# adi_keyh.add_route(mendefera, 80)
+# teseney.add_route(barentu, 100)
+# dekemhare.add_route(gindae, 50)
+akordat.add_route(teseney, 50)
+ 
+ puts dijkstra_shortest_path(asmara, teseney)

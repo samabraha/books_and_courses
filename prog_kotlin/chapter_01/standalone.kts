22f31e17b7eb7d4a280ae7bb27f3e8140ab68c0b
samabraha <samson.abraha@gmail.com>
2022-01-12 12:55:58 +0100

    Kotlin prelims

--- prog_kotlin/chapter_01/standalone.kts
+++ prog_kotlin/chapter_01/standalone.kts
@@ -32,8 +32,7 @@ val price = 12.25
 val taxRate = 0.08
 
 val output = "The amount $price after tax comes to $${price * (1 + taxRate)}"
-val disclaimer = "The amount is in US$, that's right in \$only"
-
+val disclaimer = "The amount is in US$, that's right in $ only"
 
 println(output)
 println(disclaimer)
